deb5d9187b189024da3179908da801542e109bfa
Mahammad Ali Jouhar <mahammadjouhar@gmail.com>
2026-09-22 15:50:30 +0530

    Allow username login

--- PharmaFlow/Models/ViewModels/LoginViewModel.cs
+++ PharmaFlow/Models/ViewModels/LoginViewModel.cs
@@ -4,9 +4,9 @@ namespace PharmaFlow.Models.ViewModels;
 
 public class LoginViewModel
 {
-    [Required, EmailAddress]
-    [Display(Name = "Email address")]
-    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100, MinimumLength = 2)]
+    [Display(Name = "Username")]
+    public string Username { get; set; } = string.Empty;
 
     [Required, DataType(DataType.Password)]
     [StringLength(100, MinimumLength = 8)]
