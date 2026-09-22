761e9a2f544d84adb329692bb4ff2732020e261c
Mahammad Ali Jouhar <mahammadjouhar@gmail.com>
2026-09-22 15:04:59 +0530

    Collect email and password while retaining phone number

--- PharmaFlow/Models/ViewModels/SignupViewModel.cs
+++ PharmaFlow/Models/ViewModels/SignupViewModel.cs
@@ -4,11 +4,19 @@ namespace PharmaFlow.Models.ViewModels;
 
 public class SignupViewModel
 {
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
+    [Required, StringLength(100, MinimumLength = 2)]
     public string FullName { get; set; } = string.Empty;
 
+    [Required, EmailAddress]
+    [Display(Name = "Email address")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 8)]
+    public string Password { get; set; } = string.Empty;
+
     [Required]
-    [RegularExpression(@"^[6-9][0-9]{9}$", ErrorMessage = "Enter a valid mobile number.")]
+    [RegularExpression(@"^[6-9][0-9]{9}$", ErrorMessage = "Enter a valid 10-digit Indian mobile number.")]
+    [Display(Name = "Phone number")]
     public string PhoneNumber { get; set; } = string.Empty;
 }
