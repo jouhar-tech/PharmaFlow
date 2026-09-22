0bdee9c5a167230349c60a9aa6b36c6041c7a2a1
Mahammad Ali Jouhar <mahammadjouhar@gmail.com>
2026-09-22 15:04:55 +0530

    Switch login form to email and password

--- PharmaFlow/Models/ViewModels/LoginViewModel.cs
+++ PharmaFlow/Models/ViewModels/LoginViewModel.cs
@@ -1,16 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace PharmaFlow.Models.ViewModels
+namespace PharmaFlow.Models.ViewModels;
+
+public class LoginViewModel
 {
-    public class LoginViewModel
-    {
-        [Required(ErrorMessage = "Phone number is required")]
+    [Required, EmailAddress]
+    [Display(Name = "Email address")]
+    public string Email { get; set; } = string.Empty;
 
-        [RegularExpression(
-            @"^[6-9][0-9]{9}$",
-            ErrorMessage = "Enter a valid 10-digit Indian mobile number")]
-
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; } = string.Empty;
-    }
+    [Required, DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 8)]
+    public string Password { get; set; } = string.Empty;
 }
